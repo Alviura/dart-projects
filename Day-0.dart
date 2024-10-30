feb82e0c61ae436405c0af297d77516dc5ba51a8
Alvin <alviura@outlook.com>
2024-10-30 12:13:18 +0300

    Day-0

---
+++ Day-0.dart
@@ -0,0 +1,25 @@
+// Day 0 of learning dart
+
+/* Content learnt 
+ * Introduction - print hello world
+ * Variables in dart
+ * 
+*/
+
+//Simple Hello world
+void main(){ 
+    print("Hello, world!");
+    print(1+1);
+}
+
+// In dart you can declare variables without specifing their type var.
+// Thanks to type inference, these variables types are determined by their initial values
+var name = 'Alvin';
+var year = 2024;
+var pi = 3.14;
+var planets = ['jupiter', 'mars', 'earth'];
+var image = {
+    'tags':['saturn'],
+    'url': '//path/to/saturn'
+};
+
