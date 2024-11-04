98dc48fc92f98b45c8ba17febd6de8030fac2163
Alvin <alviura@outlook.com>
2024-11-04 21:06:57 +0300

    Update Collection-0

--- Collection-0.dart
+++ Collection-0.dart
@@ -7,24 +7,28 @@
  * 
 */
 
+
+// Dart is a general purpose language created by Google designed to be simple, flexible and efficient for building both client and server side apps.
 //Simple Hello world
 void main(){ 
     print("Hello, world!");
     print(1+1);
 
-
+    // Variables 
     // In dart you can declare variables without specifing their type var.
     // Thanks to type inference, these variables types are determined by their initial values
     var name = 'Alvin';
     var year = 2024;
     var pi = 3.14;
     var planets = ['jupiter', 'mars', 'earth'];
-    var image = {
-        'tags':['saturn'],
-        'url': '//path/to/saturn'
+    var student = {
+        "Alvin": 12,
+        "John": 13,
     };
+    print(student);
 
-    // dynamic -> used if you dont know the type of variable
+    // dynamic -> used to define a variable that can hold values of any type, aand tou dont want to impose any restrictions on the type
+    // Its used to if are uncertain with the types of the calues, for instance if you are dealing with user input, JSON responces, or data form APIs
     dynamic firstName = 'Clinton';
 
     // const and final-> for variables with fixed values
@@ -52,6 +56,8 @@ void main(){
     // Dynamic 
     dynamic cityState = 'Nairobi';
     print("Dynamic: $cityState");
+
+    
 
 }
 
